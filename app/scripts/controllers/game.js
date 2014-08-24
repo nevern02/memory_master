9b09793cf94779d4eb0a778752e6264b1bea870b
Brandon Rice <brice84@gmail.com>
2014-08-24 18:34:33 -0400

    Add a winning modal directive.  Add image colors.

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,13 +1,24 @@
 MemorizeMaster.controller("GameCtrl", ["$scope", "$timeout", "$interval", "Card", "ImageList", function($scope, $timeout, $interval, Card, ImageList) {
+  var colors = ['red', 'green', 'blue', 'teal', 'orange', 'purple'];
   var NUMBER_OF_CARDS = 20;
   var imageList = new ImageList(NUMBER_OF_CARDS / 2);
-  var images = _.shuffle(imageList.images.concat(_.clone(imageList.images)));
+  var imageColorMap = _.map(imageList.images, function(image) {
+    var color = _.shuffle(colors).shift();
+    return {image: image, color: color};
+  });
+  var images = _.shuffle(imageColorMap.concat(_.clone(imageColorMap)));
+
+  $scope.modalShown = false;
   $scope.cards = [];
   $scope.currentPair = [];
   $scope.enableClick = true;
   $scope.elapsedSeconds = 0;
 
-  $interval(function() {
+  var hasWon = function() {
+    return !_.any($scope.cards, function(i) { return !i.isShowing });
+  }
+
+  var timer = $interval(function() {
     $scope.elapsedSeconds += 1;
   }, 1000);
 
@@ -16,7 +27,7 @@ MemorizeMaster.controller("GameCtrl", ["$scope", "$timeout", "$interval", "Card"
   });
 
   $scope.reveal = function(card) {
-    if (!$scope.enableClick) {
+    if (!$scope.enableClick || card.isShowing) {
       return;
     }
 
@@ -25,13 +36,21 @@ MemorizeMaster.controller("GameCtrl", ["$scope", "$timeout", "$interval", "Card"
 
     if ($scope.currentPair.length > 1) {
       $scope.enableClick = false;
-      $timeout(function() {
-        if ($scope.currentPair[0].image !== $scope.currentPair[1].image) {
+      if ($scope.currentPair[0].image !== $scope.currentPair[1].image) {
+        $timeout(function() {
           _.each($scope.currentPair, function(card) { card.isShowing = false });
-        } 
-        $scope.currentPair = [];
-        $scope.enableClick = true;
-      }, 1000);
+          $scope.currentPair = [];
+          $scope.enableClick = true;
+        }, 1000);
+      } else {
+        if (hasWon()) {
+          $interval.cancel(timer);
+          $scope.modalShown = true;
+        } else {
+          $scope.currentPair = [];
+          $scope.enableClick = true;
+        }
+      }
     }
   };
 
--- app/scripts/services/card.js
+++ app/scripts/services/card.js
@@ -1,8 +1,17 @@
 MemorizeMaster.factory("Card", [function() {
   var Card = function(image) {
     this.isShowing = false;
-    this.image = image;
+    this.image = image.image;
+    this.color = image.color;
   };
+
+  Card.prototype.currentColor = function() {
+    if (this.isShowing) {
+      return this.color;
+    } else {
+      return 'transparent';
+    }
+  }
 
   return Card;
 }]);
