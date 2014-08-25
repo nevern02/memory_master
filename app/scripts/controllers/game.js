4682f94c75132dfc81c46314af79abfdd17ae1d4
Brandon Rice <brice84@gmail.com>
2014-08-24 20:43:04 -0400

    Add multiple stages.

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,30 +1,48 @@
 MemorizeMaster.controller("GameCtrl", ["$scope", "$timeout", "$interval", "Card", "ImageList", function($scope, $timeout, $interval, Card, ImageList) {
+  var imageList = null;
+  var imageColorMap = null;
+  var images = null;
+  var timer = null;
+  var numberOfCards = 10;
   var colors = ['red', 'green', 'blue', 'teal', 'orange', 'purple'];
-  var NUMBER_OF_CARDS = 20;
-  var imageList = new ImageList(NUMBER_OF_CARDS / 2);
-  var imageColorMap = _.map(imageList.images, function(image) {
-    var color = _.shuffle(colors).shift();
-    return {image: image, color: color};
-  });
-  var images = _.shuffle(imageColorMap.concat(_.clone(imageColorMap)));
 
-  $scope.modalShown = false;
-  $scope.cards = [];
-  $scope.currentPair = [];
-  $scope.enableClick = true;
-  $scope.elapsedSeconds = 0;
+  $scope.stage = 1;
+  $scope.bestScores = {};
+
+  var initializeStage = function() {
+    $scope.currentPair = [];
+    $scope.cards = [];
+    $scope.elapsedSeconds = 0;
+
+    imageList = new ImageList(numberOfCards / 2);
+    imageColorMap = _.map(imageList.images, function(image) {
+      var color = _.shuffle(colors).shift();
+      return {image: image, color: color};
+    });
+    images = _.shuffle(imageColorMap.concat(_.clone(imageColorMap)));
+
+    timer = $interval(function() {
+      $scope.elapsedSeconds += 1;
+    }, 1000);
+
+    _.each(images, function(image) {
+      $scope.cards.push(new Card(image));
+    });
+
+    $scope.enableClick = true;
+  }
+  initializeStage();
 
   var hasWon = function() {
     return !_.any($scope.cards, function(i) { return !i.isShowing });
   }
 
-  var timer = $interval(function() {
-    $scope.elapsedSeconds += 1;
-  }, 1000);
-
-  _.each(images, function(image) {
-    $scope.cards.push(new Card(image));
-  });
+  $scope.nextStage = function() {
+    $scope.stage += 1;
+    numberOfCards += 2;
+    initializeStage();
+    $scope.modalShown = false;
+  }
 
   $scope.reveal = function(card) {
     if (!$scope.enableClick || card.isShowing) {
