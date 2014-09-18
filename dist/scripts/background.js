a090258adfc918cc27b581775405d3afee5f199a
Brandon Rice <brice84@gmail.com>
2014-09-17 23:24:11 -0400

    Add dist to pages branch.

---
+++ dist/scripts/background.js
@@ -0,0 +1,1 @@
+"use strict";chrome.app.runtime.onLaunched.addListener(function(){var a=1024,b=768;chrome.app.window.create("index.html",{bounds:{width:a,height:b,left:Math.round((screen.availWidth-a)/2),top:Math.round((screen.availHeight-b)/2)}})});
