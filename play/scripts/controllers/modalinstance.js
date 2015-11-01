d969221bfc4ea2d8aebfed6ae8367396833bfbe0
Brandon Rice <brice84@gmail.com>
2015-11-01 18:50:20 -0500

    Remove unneeded files for web target.

--- play/scripts/controllers/modalinstance.js
+++
@@ -1,9 +0,0 @@
-MemoryMaster.controller('ModalInstanceCtlr', ['$scope', '$modalInstance', function($scope, $modalInstance) {
-  $scope.close = function() {
-    $modalInstance.close();
-  }
-
-  $scope.dismiss = function() {
-    $modalInstance.dismiss();
-  }
-}]);
